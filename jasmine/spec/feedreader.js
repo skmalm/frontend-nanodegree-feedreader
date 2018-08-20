7f68bf56cbf985553d54ca07f3dead837de50ca0
Sebastian-Malm <skmalm@gmail.com>
2018-08-20 17:05:21 -0400

    style: make sure everything uses consistent 2-space indentation

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,69 +9,69 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    describe('RSS Feeds', function() {
-        /* This test makes sure that the allFeeds variable has been defined
-         * and that it is not empty.
-         */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
-
-
-        /* This test loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('have defined URLs', function() {
-           for (let i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].url).toBeDefined();
-             expect(allFeeds[i].url.length).not.toBe(0);
-           }
-         })
-
-
-        /* This test loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-         it('have defined names', function() {
-           for (let i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].name).toBeDefined();
-             expect(allFeeds[i].name.length).not.toBe(0);
-           }
-         })
+  describe('RSS Feeds', function() {
+    /* This test makes sure that the allFeeds variable has been defined
+     * and that it is not empty.
+     */
+    it('are defined', function() {
+        expect(allFeeds).toBeDefined();
+        expect(allFeeds.length).not.toBe(0);
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+    /* This test loops through each feed
+     * in the allFeeds object and ensures it has a URL defined
+     * and that the URL is not empty.
+     */
+     it('have defined URLs', function() {
+       for (let i = 0; i < allFeeds.length; i++) {
+         expect(allFeeds[i].url).toBeDefined();
+         expect(allFeeds[i].url.length).not.toBe(0);
+       }
+     })
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* This test loops through each feed
+     * in the allFeeds object and ensures it has a name defined
+     * and that the name is not empty.
+     */
+     it('have defined names', function() {
+       for (let i = 0; i < allFeeds.length; i++) {
+         expect(allFeeds[i].name).toBeDefined();
+         expect(allFeeds[i].name.length).not.toBe(0);
+       }
+     })
+  });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+  /* TODO: Write a new test suite named "The menu" */
+  describe('The menu', function() {
+    /* TODO: Write a test that ensures the menu element is
+     * hidden by default. You'll have to analyze the HTML and
+     * the CSS to determine how we're performing the
+     * hiding/showing of the menu element.
+     */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
-    });
+     /* TODO: Write a test that ensures the menu changes
+      * visibility when the menu icon is clicked. This test
+      * should have two expectations: does the menu display when
+      * clicked and does it hide when clicked again.
+      */
+
+  /* TODO: Write a new test suite named "Initial Entries" */
+
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+
+  /* TODO: Write a new test suite named "New Feed Selection" */
+
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
+  });
 }());
