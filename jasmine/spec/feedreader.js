b64a98c39a5df6c396a5d5c65079e795d2868a72
Sebastian-Malm <skmalm@gmail.com>
2018-08-20 17:52:55 -0400

    test: add 'Initial Entries' suite; no specs added yet

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,10 +61,10 @@ $(function() {
         // click the menu icon once more to hide the menu
         $('.menu-icon-link').trigger('click');
         expect($('body').hasClass('menu-hidden')).toBe(true);
-      })
-
+      });
+  });
   /* TODO: Write a new test suite named "Initial Entries" */
-
+  describe('Initial Entries', function() {
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
