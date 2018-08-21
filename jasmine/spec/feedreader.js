e46f6ea2df0a54e7f721002ba824e627ac6eef6b
Sebastian-Malm <skmalm@gmail.com>
2018-08-20 20:10:34 -0400

    test: add 'Initial Entries should have at least one article' spec and c/u comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -63,14 +63,23 @@ $(function() {
         expect($('body').hasClass('menu-hidden')).toBe(true);
       });
   });
-  /* TODO: Write a new test suite named "Initial Entries" */
+
   describe('Initial Entries', function() {
-    /* TODO: Write a test that ensures when the loadFeed
+    /* This test ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
      */
+
+     beforeEach(function(done) {
+       loadFeed(0, function() {
+         done();
+       });
+     });
+
+     it('should have at least one article', function(done) {
+       expect($('.entry').length).not.toBe(0);
+       done();
+     });
 
   /* TODO: Write a new test suite named "New Feed Selection" */
 
